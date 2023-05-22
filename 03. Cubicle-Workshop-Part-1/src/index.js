fae0186d6d730c443e9397b303bf13551ae33969
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2023-05-22 18:56:39 +0300

    added static assets

--- 03. Cubicle-Workshop-Part-1/src/index.js
+++ 03. Cubicle-Workshop-Part-1/src/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 5000;
 
+//css middleware
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+//Handlebars
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');// set the views folder directory
+
+
+//Routers
 app.get('/', (req, res)=>{
-    res.send('hi')
+    res.render('index')
 })
 
 app.listen(port, ()=> console.log(`Server is running on port ${port}`));
