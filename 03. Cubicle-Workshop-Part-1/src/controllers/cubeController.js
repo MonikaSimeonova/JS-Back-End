09bdb66806421171a2759518e168e36b14d19baa
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2023-05-22 21:00:26 +0300

    posting cubes

--- 03. Cubicle-Workshop-Part-1/src/controllers/cubeController.js
+++ 03. Cubicle-Workshop-Part-1/src/controllers/cubeController.js
@@ -8,11 +8,11 @@ router.get('/create', (req, res) => {
     res.render('create')
 });
 
-router.post('/create', (req, res)=>{
-    const {name, description, imageUrl, difficultyLevel} = req.body;
+router.post('/create', (req, res) => {
+    const { name, description, imageUrl, difficultyLevel } = req.body;
 
-    cubeManager.create({name, description, imageUrl, difficultyLevel: Number(difficultyLevel)})
-    
+    cubeManager.create({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel) })
+
     res.redirect('/');
 });
 
