5b4aefee0c17d739bd5b0ad0f461563c2fb31cf6
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2023-06-02 20:10:09 +0300

    show accessories option menu

--- 04. Cubicle-Workshop-Part-2/src/controllers/cubeController.js
+++ 04. Cubicle-Workshop-Part-2/src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
-const cubeManager = require('../managers/cubeManager')
+const cubeManager = require('../managers/cubeManager');
+const accessoryManager = require('../managers/accessoryManager');
 
 //path /cubes/create
 router.get('/create', (req, res) => {
@@ -11,15 +12,16 @@ router.get('/create', (req, res) => {
 router.post('/create', async (req, res) => {
     const { name, description, imageUrl, difficultyLevel } = req.body;
 
-   await cubeManager.create({ 
-    name, 
-    description, 
-    imageUrl, 
-    difficultyLevel: Number(difficultyLevel) })
+    await cubeManager.create({
+        name,
+        description,
+        imageUrl,
+        difficultyLevel: Number(difficultyLevel)
+    })
 
     res.redirect('/');
 });
-router.get('/:cubeId/details', async(req, res) => {
+router.get('/:cubeId/details', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
 
     if (!cube) {
@@ -27,6 +29,14 @@ router.get('/:cubeId/details', async(req, res) => {
     }
     res.render('details', { cube });
 });
+
+router.get('/:cubeId/attach-accessory', async(req, res) => {
+
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryManager.getAll().lean();
+
+    res.render('accessory/attach', {cube, accessories});
+})
 
 
 module.exports = router;
