568ff4a9ec3ede08c3bc8d4156d5453ff7037a04
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2023-06-09 22:16:39 +0300

    delete from db

--- 05. Sessions-and-Authentication-Workshop/src/controllers/cubeController.js
+++ 05. Sessions-and-Authentication-Workshop/src/controllers/cubeController.js
@@ -53,7 +53,13 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
 
 router.get('/:cubeId/delete',async(req, res)=>{
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
-    res.render('cube/delete', {cube})
+    res.render('cube/delete', {cube});
+});
+
+router.post('/:cubeId/delete',async(req, res)=>{
+   await cubeManager.delete(req.params.cubeId);
+
+   res.redirect('/')
 })
 
 
