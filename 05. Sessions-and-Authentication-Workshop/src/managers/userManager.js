c5a59c22c74990288dd37dffbb47b2f96eb7bd68
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2023-06-09 21:14:13 +0300

    create login template

--- 05. Sessions-and-Authentication-Workshop/src/managers/userManager.js
+++ 05. Sessions-and-Authentication-Workshop/src/managers/userManager.js
@@ -1,3 +1,24 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+    //find user
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Invalid username or password');
+    }
+    //validate password
+    const isValid = await bcrypt.compare(password, user.password);
+    
+    if (!isValid) {
+        throw new Error('Invalid username or password');
+    }
+
+    
+
+    //TODO return user
+    return user
+};
