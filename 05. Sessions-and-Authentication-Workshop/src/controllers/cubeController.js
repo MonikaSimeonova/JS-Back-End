91ec833d2e4362555be2dd3fbc547d54058d2558
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2023-06-11 11:09:17 +0300

    add generate options from diffLevel

--- 05. Sessions-and-Authentication-Workshop/src/controllers/cubeController.js
+++ 05. Sessions-and-Authentication-Workshop/src/controllers/cubeController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const cubeManager = require('../managers/cubeManager');
 const accessoryManager = require('../managers/accessoryManager');
-
+const {getDifficultyOptionsViewData} = require('../utils/viewHelpers');
 //path /cubes/create
 router.get('/create', (req, res) => {
     //console.log(req.user);
@@ -19,7 +19,7 @@ router.post('/create', async (req, res) => {
         imageUrl,
         difficultyLevel: Number(difficultyLevel),
         owner: req.user._id
-    })
+    });
 
     res.redirect('/');
 });
@@ -40,7 +40,7 @@ router.get('/:cubeId/attach-accessory', async (req, res) => {
     const hasAccessories = accessories.length > 0;
 
     res.render('accessory/attach', { cube, accessories, hasAccessories });
-})
+});
 router.post('/:cubeId/attach-accessory', async (req, res) => {
     const { accessory: accessoryId } = req.body;
     const cubeId = req.params.cubeId;
@@ -53,17 +53,24 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
 
 router.get('/:cubeId/delete', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
-    res.render('cube/delete', { cube });
+    const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+
+    res.render('cube/delete', { cube, options });
 });
 
 router.post('/:cubeId/delete', async (req, res) => {
     await cubeManager.delete(req.params.cubeId);
 
     res.redirect('/')
-})
+});
+
+
 router.get('/:cubeId/edit', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
-    res.render('cube/edit', { cube });
+
+    const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+
+    res.render('cube/edit', { cube, options });
 });
 router.post('/:cubeId/edit', async (req, res) => {
     const cubeData = req.body;
