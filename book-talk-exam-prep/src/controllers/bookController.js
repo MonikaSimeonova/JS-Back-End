b4085becebe23709fd0990c80757c3997ea89d07
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2023-06-15 21:08:03 +0300

    edit view

--- book-talk-exam-prep/src/controllers/bookController.js
+++ book-talk-exam-prep/src/controllers/bookController.js
@@ -21,5 +21,24 @@ router.get('/catalog', async(req, res) => {
     const books = await bookManager.getAll().lean();
     res.render('catalog', {books})
 });
+//details
+router.get('/:bookId/details', async(req, res) => {
+    const book = await bookManager.findOne(req.params.bookId);
+    console.log(book);
+    const isOwner = req.user?._id == book.owner._id;
+    console.log(isOwner);
+    res.render('details', {book, isOwner})
+});
+//delete
+router.get('/:bookId/delete', async(req, res) => {
+    await bookManager.delete(req.params.bookId);
+    res.redirect('/books/catalog')
+});
+//edit
+router.get('/:bookId/edit', async(req, res) => {
+    const book = await bookManager.findOne(req.params.bookId);
+    res.render('edit', {book})
+});
+
 
 module.exports = router;
--- book-talk-exam-prep/src/managers/bookManager.js
+++ book-talk-exam-prep/src/managers/bookManager.js
@@ -2,3 +2,5 @@ const Book = require('../models/Book');
 
 exports.create = (bookData)=> Book.create(bookData);
 exports.getAll = ()=> Book.find();
+exports.findOne = (bookId)=> Book.findById(bookId).lean().populate('owner');
+exports.delete = (bookId)=> Book.findByIdAndDelete(bookId);
